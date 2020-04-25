b11513bd1fd82066eb137e0f5276c6bc8875533b
israel <israel.bejarano.a@gmail.com>
2020-04-25 12:47:42 +0200

    entidad curso

---
+++ microservicios-cursos/src/main/java/com/curso/microservicios/app/cursos/models/entity/Curso.java
@@ -0,0 +1,61 @@
+package com.curso.microservicios.app.cursos.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "cursos")
+public class Curso implements Serializable {
+
+	private static final long serialVersionUID = -4627078158771939317L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createAt;
+
+	@PrePersist
+	public void prePersist() {
+		this.createAt = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+}
