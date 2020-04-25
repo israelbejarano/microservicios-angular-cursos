dc8447287498297346b86c9dd384b1040ef78460
israel <israel.bejarano.a@gmail.com>
2020-04-25 13:21:32 +0200

    entityScan de alumnos y cursos

--- microservicios-cursos/src/main/java/com/curso/microservicios/app/cursos/MicroserviciosCursosApplication.java
+++ microservicios-cursos/src/main/java/com/curso/microservicios/app/cursos/MicroserviciosCursosApplication.java
@@ -2,10 +2,13 @@ package com.curso.microservicios.app.cursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.curso.microservicios.commons.alumnos.models.entity",
+			 "com.curso.microservicios.app.cursos.models.entity"})
 public class MicroserviciosCursosApplication {
 
 	public static void main(String[] args) {
