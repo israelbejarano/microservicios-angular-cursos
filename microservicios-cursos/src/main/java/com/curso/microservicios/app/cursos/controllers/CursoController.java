808e122d866ce2aa3c1951ee48747ae69823d043
israel <israel.bejarano.a@gmail.com>
2020-04-25 13:01:13 +0200

    controlador para curso

---
+++ microservicios-cursos/src/main/java/com/curso/microservicios/app/cursos/controllers/CursoController.java
@@ -0,0 +1,30 @@
+package com.curso.microservicios.app.cursos.controllers;
+
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.microservicios.app.cursos.models.entity.Curso;
+import com.curso.microservicios.app.cursos.services.CursoService;
+import com.curso.microservicios.commons.controllers.CommonController;
+
+@RestController
+public class CursoController extends CommonController<Curso, CursoService> {
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> editar(@RequestBody Curso curso, @PathVariable Long id) {
+		Optional<Curso> o = this.service.findById(id);
+		if (!o.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		Curso cursoDb = o.get();
+		cursoDb.setNombre(curso.getNombre());
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(cursoDb));
+	}
+
+}
