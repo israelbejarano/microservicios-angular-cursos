cec9ad0328789650cf4aec66b4271c8169cdad3d
israel <israel.bejarano.a@gmail.com>
2020-04-26 12:00:58 +0200

    Usando join para obtener el curso asignado al alumno

--- microservicios-cursos/src/main/java/com/curso/microservicios/app/cursos/controllers/CursoController.java
+++ microservicios-cursos/src/main/java/com/curso/microservicios/app/cursos/controllers/CursoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -51,6 +52,12 @@ public class CursoController extends CommonController<Curso, CursoService> {
 		Curso cursoDb = o.get();
 		cursoDb.removeAlumno(alumno);
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(cursoDb));
+	}
+	
+	@GetMapping("/alumno/{id}")
+	public ResponseEntity<?> buscarPorAlumnoId(@PathVariable Long id) {
+		Curso curso = service.findCursoByAlumnoId(id);
+		return ResponseEntity.ok(curso);
 	}
 
 }
--- microservicios-cursos/src/main/java/com/curso/microservicios/app/cursos/services/CursoService.java
+++ microservicios-cursos/src/main/java/com/curso/microservicios/app/cursos/services/CursoService.java
@@ -4,5 +4,7 @@ import com.curso.microservicios.app.cursos.models.entity.Curso;
 import com.curso.microservicios.commons.services.CommonService;
 
 public interface CursoService extends CommonService<Curso> {
+	
+	public Curso findCursoByAlumnoId(Long id);
 
 }
