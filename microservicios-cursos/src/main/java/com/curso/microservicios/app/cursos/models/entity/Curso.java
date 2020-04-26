62ed06dfdbdda5e64b4cdaf6d0b0642d750c42d4
israel <israel.bejarano.a@gmail.com>
2020-04-26 11:11:34 +0200

    Añadiendo relación en curso con sus alumnos

--- microservicios-cursos/src/main/java/com/curso/microservicios/app/cursos/models/entity/Curso.java
+++ microservicios-cursos/src/main/java/com/curso/microservicios/app/cursos/models/entity/Curso.java
@@ -1,17 +1,23 @@
 package com.curso.microservicios.app.cursos.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import com.curso.microservicios.commons.alumnos.models.entity.Alumno;
 
 @Entity
 @Table(name = "cursos")
@@ -22,16 +28,23 @@ public class Curso implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String nombre;
-	
+
 	@Column(name = "create_at")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
 
+	@OneToMany(fetch = FetchType.LAZY) // carga perezosa
+	private List<Alumno> alumnos;
+
 	@PrePersist
 	public void prePersist() {
 		this.createAt = new Date();
+	}
+
+	public Curso() {
+		this.alumnos = new ArrayList<>();
 	}
 
 	public Long getId() {
@@ -56,6 +69,22 @@ public class Curso implements Serializable {
 
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+
+	public List<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(List<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
+
+	public void addAlumno(Alumno alumno) {
+		this.alumnos.add(alumno);
+	}
+	
+	public void removeAlumno(Alumno alumno) {
+		this.alumnos.remove(alumno);
 	}
 
 }
