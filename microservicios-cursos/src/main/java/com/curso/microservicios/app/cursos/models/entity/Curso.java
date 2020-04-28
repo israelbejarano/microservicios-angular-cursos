b44ec2db391033dca62f903d9cd9ba3c594092bc
israel <israel.bejarano.a@gmail.com>
2020-04-28 11:58:50 +0200

    validacion

--- microservicios-cursos/src/main/java/com/curso/microservicios/app/cursos/models/entity/Curso.java
+++ microservicios-cursos/src/main/java/com/curso/microservicios/app/cursos/models/entity/Curso.java
@@ -17,6 +17,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
 
 import com.curso.microservicios.commons.alumnos.models.entity.Alumno;
 import com.curso.microservicios.commons.examenes.models.entity.Examen;
@@ -31,6 +32,7 @@ public class Curso implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotEmpty
 	private String nombre;
 
 	@Column(name = "create_at")
